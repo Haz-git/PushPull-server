a08281310f4433e385275ba9da468660338ebe30
Haz-git <zhouharry1@gmail.com>
2021-06-21 14:40:07 -0700

    Configured TS for nodeJS server, set up postgres server as well

---
+++ server.tsx
@@ -0,0 +1,29 @@
+export {};
+
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:8081',
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const db = require('./app/models');
+db.sync();
+
+app.get('/', (req: any, res: any) => {
+    res.json({ message: 'Hello World!' });
+});
+
+const PORT = process.env.PORT || 8080;
+
+//routes
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});

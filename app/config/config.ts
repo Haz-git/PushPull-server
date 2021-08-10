f2fcf02da2ccd5bba145beac10489299dac068ca
Haz-git <zhouharry1@gmail.com>
2021-08-10 12:03:05 -0700

    Modified config to try to enable push to DB

--- app/config/config.ts
+++ app/config/config.ts
@@ -6,13 +6,9 @@ module.exports = {
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
         host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
         dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
+        dialectOptions: {},
         operatorsAliases: false,
         pool: {
             max: 5,
@@ -33,6 +29,7 @@ module.exports = {
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
         host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
         dialect: 'postgres',
         dialectOptions: {
             ssl: {

f89b77d563c14f56ebb6b7b2c18a67ae5b406768
Haz-git <zhouharry1@gmail.com>
2021-08-09 18:42:33 -0700

    Created index model for postgres

--- app/models/index.ts
+++ app/models/index.ts
@@ -1,28 +1,36 @@
-const dbConfig = require('../config/db.config');
+'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        },
-    },
-    operatorsAliases: false,
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config.js')[env];
+const db: any = {};
 
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle,
-    },
+let sequelize: any;
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs.readdirSync(__dirname)
+    .filter((file: any) => {
+        return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.ts';
+    })
+    .forEach((file: any) => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
-// const db = {};
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-// db.Sequelize = Sequelize;
-// db.sequelize = sequelize;
-
-module.exports = sequelize;
+module.exports = db;

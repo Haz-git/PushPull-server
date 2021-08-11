814055c54843572a08c090529215889e1292d34f
Haz-git <zhouharry1@gmail.com>
2021-08-11 08:45:00 -0700

    Added test route

--- app/controllers/categoryController.ts
+++ app/controllers/categoryController.ts
@@ -1,10 +1,18 @@
+//Express Types:
+import { Request, Response } from 'express';
+
 //Model:
 const categoryModel = require('../models/category');
 
 //Utilities:
 const handleAsyncError = require('../utils/handleAsyncErrors');
 
-exports.getAll = handleAsyncError(async (req: Request, res: Response, next: any) => {});
+exports.getAll = handleAsyncError(async (req: Request, res: Response, next: any) => {
+    return res.status(200).json({
+        status: 'Success',
+        msg: 'Route established',
+    });
+});
 
 exports.getOne = handleAsyncError(async (req: Request, res: Response, next: any) => {});
 

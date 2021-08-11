cd98cd82c7032b1a5938e5cb959fbe8c4649754f
Haz-git <zhouharry1@gmail.com>
2021-08-11 08:20:49 -0700

    Added template controller functions

---
+++ app/controllers/categoryController.ts
@@ -0,0 +1,15 @@
+//Model:
+const categoryModel = require('../models/category');
+
+//Utilities:
+const handleAsyncError = require('../utils/handleAsyncErrors');
+
+exports.getAll = handleAsyncError(async (req: Request, res: Response, next: any) => {});
+
+exports.getOne = handleAsyncError(async (req: Request, res: Response, next: any) => {});
+
+exports.createOne = handleAsyncError(async (req: Request, res: Response, next: any) => {});
+
+exports.updateOne = handleAsyncError(async (req: Request, res: Response, next: any) => {});
+
+exports.deleteOne = handleAsyncError(async (req: Request, res: Response, next: any) => {});

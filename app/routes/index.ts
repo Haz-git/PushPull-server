761de9d17bc9b7ed035345e2b741ae7adb250e05
Haz-git <zhouharry1@gmail.com>
2021-08-11 08:15:06 -0700

    Commenting for future development

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -3,3 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 //Controllers:
+const categoryController = require('../controllers/categoryController');
+const exerciseController = require('../controllers/exerciseController');
+
+//Category Routes:
+
+// router
+//     .get('/categories/all', categoryController.getAll)
+//     .get('/categories/:id', categoryController.getOne)
+//     .post('/categories/create', categoryController.createOne)
+//     .put('/categories/update/:id', categoryController.updateOne)
+//     .delete('/categories/delete/:id', categoryController.deleteOne);
+
+//Exercise Routes:

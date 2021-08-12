a0fef11ef3f8a427cb030b3f0d9aacaf431e72c5
Haz-git <zhouharry1@gmail.com>
2021-08-12 06:52:01 -0700

    Added controller functions to GET a specific model and to CREATE a specific model

--- app/controllers/categoryController.ts
+++ app/controllers/categoryController.ts
@@ -4,6 +4,8 @@ import { Request, Response } from 'express';
 //Model:
 const db = require('../models');
 
+/* Since we are using the model loader via index.ts (this loader passes all models with sequelize + sequelize.DataTypes)into all of our models, we are importing the database and extracting our specific model via a capitalized key.*/
+
 //Utilities:
 const handleAsyncError = require('../utils/handleAsyncErrors');
 
@@ -16,9 +18,29 @@ exports.getAll = handleAsyncError(async (req: Request, res: Response, next: any)
     });
 });
 
-exports.getOne = handleAsyncError(async (req: Request, res: Response, next: any) => {});
+exports.getOne = handleAsyncError(async (req: Request, res: Response, next: any) => {
+    const specificCategory = await db.Category.findByPk(req.params.id);
 
-exports.createOne = handleAsyncError(async (req: Request, res: Response, next: any) => {});
+    return res.status(200).json({
+        status: 'Success',
+        categories: specificCategory,
+    });
+});
+
+exports.createOne = handleAsyncError(async (req: Request, res: Response, next: any) => {
+    const { categoryTitle } = req.body;
+
+    const CATEGORY_MODEL = {
+        categoryTitle: categoryTitle,
+    };
+
+    const Categories = await db.Category.create(CATEGORY_MODEL);
+
+    return res.status(200).json({
+        status: 'Success',
+        categories: Categories,
+    });
+});
 
 exports.updateOne = handleAsyncError(async (req: Request, res: Response, next: any) => {});
 

158cde1279223cd9b13890a5e004ca0142d3c345
Haz-git <zhouharry1@gmail.com>
2021-08-22 09:15:07 -0700

    Added more items into workout programs

--- app/models/workoutProgram.ts
+++ app/models/workoutProgram.ts
@@ -5,12 +5,24 @@ import { Model, UUIDV4 } from 'sequelize';
 interface workoutProgramAttributes {
     id: string;
     workoutProgramTitle: string;
+    workoutProgramDesc: string;
+    equipment: string;
+    difficulty: string;
+    workoutSchedule: string;
+    workoutLength: string;
+    rating: number;
 }
 
 module.exports = (sequelize: any, DataTypes: any) => {
     class workoutProgram extends Model<workoutProgramAttributes> implements workoutProgramAttributes {
         public id!: string;
         public workoutProgramTitle!: string;
+        public workoutProgramDesc!: string;
+        public equipment!: string;
+        public difficulty!: string;
+        public workoutSchedule!: string;
+        public workoutLength!: string;
+        public rating!: number;
 
         static associate(models: any) {
             // define association here
@@ -27,6 +39,30 @@ module.exports = (sequelize: any, DataTypes: any) => {
             },
             workoutProgramTitle: {
                 type: DataTypes.STRING,
+                allowNull: false,
+            },
+            workoutProgramDesc: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+            },
+            equipment: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            difficulty: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            workoutSchedule: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            workoutLength: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            rating: {
+                type: DataTypes.INTEGER,
                 allowNull: false,
             },
         },

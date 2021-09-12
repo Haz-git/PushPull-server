40af568fa7b82846f5c14bcf6eef362174bffea0
Haz-git <zhouharry1@gmail.com>
2021-09-12 08:09:36 -0700

    Added route for reviews

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 //Controllers:
 const categoryController = require('../controllers/categoryController');
 const workoutProgramController = require('../controllers/workoutProgramController');
+const reviewController = require('../controllers/reviewController');
 
 //Category Routes:
 
@@ -24,5 +25,8 @@ router
     .post('/workoutProgram/create', workoutProgramController.createOne)
     .put('/workoutProgram/update/:id', workoutProgramController.updateOne)
     .delete('/workoutProgram/delete/:id', workoutProgramController.deleteOne);
+
+//Review Routes:
+router.get('/reviews/all/:id', reviewController.findReviews);
 
 module.exports = router;

970c813f380e3fcadbaf89041324a84c4e8bc800
Haz <zhouharry1@gmail.com>
2021-11-02 06:24:53 -0700

    Added route for updating review votes

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -3,9 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 //Controllers:
+const AuthController = require('../controllers/authController');
 const categoryController = require('../controllers/categoryController');
 const workoutProgramController = require('../controllers/workoutProgramController');
 const reviewController = require('../controllers/reviewController');
+
+//Authentication Routes:
+router.get('/user/details', AuthController.authenticateJWT);
 
 //Category Routes:
 
@@ -22,11 +26,14 @@ router
     .post('/workoutProgram/search/:id', workoutProgramController.findNearby)
     .post('/workoutProgram/all', workoutProgramController.getAll)
     .get('/workoutProgram/:id', workoutProgramController.getOne)
-    .post('/workoutProgram/create', workoutProgramController.createOne)
+    .post('/workoutProgram/create', workoutProgramController.createsOne)
     .put('/workoutProgram/update/:id', workoutProgramController.updateOne)
     .delete('/workoutProgram/delete/:id', workoutProgramController.deleteOne);
 
 //Review Routes:
-router.get('/reviews/all/:id', reviewController.findReviews).post('/reviews/submit/:id', reviewController.addReview);
+router
+    .get('/reviews/all/:id', reviewController.findReviews)
+    .post('/reviews/submit/:id', reviewController.addReview)
+    .post('/reviews/update_votes/:id', reviewController.updateReviewVote);
 
 module.exports = router;

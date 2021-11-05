1b859c6adfc74bfe02e53ad558c6a7a6fb45029e
Haz-git <zhouharry1@gmail.com>
2021-11-05 14:48:21 -0700

    Created request submission to userfront

--- app/controllers/profileController.ts
+++ app/controllers/profileController.ts
@@ -7,8 +7,38 @@ const userfrontApi = require('../api/userfrontApi');
 const handleAsyncError = require('../utils/handleAsyncErrors');
 
 exports.findUser = handleAsyncError(async (req: Request, res: Response, next: any) => {
-    let username = req.params.id;
+    let username = req.params.username;
 
     if (username) {
+        const payload = {
+            filters: {
+                conjunction: 'and',
+                filterGroups: [
+                    {
+                        conjunction: 'and',
+                        filters: [
+                            {
+                                attr: 'username',
+                                type: 'string',
+                                comparison: 'contains',
+                                value: `${username}`,
+                            },
+                        ],
+                    },
+                ],
+            },
+        };
+
+        let response = await userfrontApi.post('/v0/users/find', payload);
+
+        return res.status(200).json({
+            status: 'Success',
+            userProfile: response,
+        });
     }
+
+    return res.status(500).json({
+        status: 'Failure',
+        msg: 'User could not be found',
+    });
 });

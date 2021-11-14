3c1baecf24c5378b2e220733a8f02b949533e139
Haz <zhouharry1@gmail.com>
2021-11-14 07:55:54 -0800

    Increased json cap

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(helmet());
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(xss());

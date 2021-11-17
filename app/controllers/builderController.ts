c2e1c93275f02712943a69f1d326f25ac586d3fd
Haz <zhouharry1@gmail.com>
2021-11-17 07:29:19 -0800

    Added controller function for grabbing user builder information

--- app/controllers/builderController.ts
+++ app/controllers/builderController.ts
@@ -7,7 +7,27 @@ const userfrontApi = require('../api/userfrontApi');
 const handleAsyncError = require('../utils/handleAsyncErrors');
 
 exports.findUserBuilderInfo = handleAsyncError(async (req: any, res: Response, next: any) => {
-    let userId = req.params.userId;
+    const { userId } = req.auth;
+
+    if (userId) {
+        let currUser = await userfrontApi.get(`/v0/users/${userId}`);
+
+        if (currUser) {
+            const { builder } = currUser?.data?.data;
+
+            if (builder) {
+                return res.status(200).json({
+                    status: 'Success',
+                    builder: builder,
+                });
+            }
+        }
+    }
+
+    return res.status(500).json({
+        status: 'Failure',
+        msg: 'User builder information could not be found',
+    });
 });
 
 exports.addProject = handleAsyncError(async (req: any, res: Response, next: any) => {});

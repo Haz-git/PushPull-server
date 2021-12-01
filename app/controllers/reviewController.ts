6b10e67fb8d75e2e29dc9db341d60528ed1cf87a
Haz <zhouharry1@gmail.com>
2021-12-01 08:18:08 -0800

    Removed unnecessary operator

--- app/controllers/reviewController.ts
+++ app/controllers/reviewController.ts
@@ -5,7 +5,6 @@ const updateWorkoutProgramOnReviewAdd = require('../utils/updateWorkoutProgramOn
 //Model
 
 const db = require('../models');
-const { Op } = require('sequelize');
 
 //Utilities:
 const handleAsyncError = require('../utils/handleAsyncErrors');

5eec099e4f665cd3cb3cb4852e67de07a4988d93
Haz-git <zhouharry1@gmail.com>
2021-12-10 08:31:19 -0800

    Created update template function

--- app/controllers/templateController.ts
+++ app/controllers/templateController.ts
@@ -101,6 +101,55 @@ exports.addTemplate = handleAsyncError(async (req: any, res: Response, next: any
 
 exports.updateTemplate = handleAsyncError(async (req: any, res: Response, next: any) => {
     const { userId } = req.auth;
+    const { templateDetails } = req.body;
+    let templateId = req.params.templateId;
+
+    let newTemplateDetails = { ...templateDetails };
+    newTemplateDetails.updatedDate = new Date();
+
+    if (userId && templateId) {
+        let currUser = await userfrontApi.get(`/v0/users/${userId}`);
+
+        try {
+            let targetTemplate = await db.templateFile.findOne({
+                where: {
+                    id: templateId,
+                },
+            });
+
+            await targetTemplate.update(newTemplateDetails);
+            await targetTemplate.save();
+
+            let totalTemplates = await db.templateFile.findAll({
+                where: {
+                    templateCreatedBy: {
+                        [Op.contains]: {
+                            userfrontUserId: `${userId}`,
+                            username: currUser.data.username,
+                            userImage: currUser.data.image,
+                        },
+                    },
+                },
+                order: ['createdAt'],
+            });
+
+            if (totalTemplates) {
+                return res.status(200).json({
+                    status: 'Success',
+                    templates: totalTemplates,
+                });
+            }
+        } catch (err) {
+            return res.status(500).json({
+                status: 'Failed',
+                msg: 'An error occurred retrieving user templates',
+            });
+        }
+    }
+    return res.status(500).json({
+        status: 'Failed',
+        msg: 'An error occurred--no credentials provided',
+    });
 });
 
 exports.deleteTemplate = handleAsyncError(async (req: any, res: Response, next: any) => {

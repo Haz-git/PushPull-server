3156e7990dac436c40ca6906d4b648148d073349
Harry Zhou <haz@its-bio-144-232.ucsd.edu>
2022-03-29 15:38:06 -0700

    Added tester function for gettingUsers

--- app/controllers/userController.ts
+++ app/controllers/userController.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from 'express';
 const jwt = require('jsonwebtoken');
+const userfrontApi = require('../api/userfrontApi');
 const handleAsyncError = require('../utils/handleAsyncErrors');
 
 //look into userFront API to find request to get user data....
 
-exports.findUser = handleAsyncError(async (req: Request, res: Response, next: any) => {});
+exports.getUser = handleAsyncError(async (req: any, res: Response, next: any) => {
+    const { userId } = req.auth;
+
+    try {
+        const response = await userfrontApi.get(`/v0/users/${userId}`);
+        console.log(response);
+
+        return res.status(200).json({
+            status: 'Success',
+            user: response.data,
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            status: 'Fail',
+            msg: 'An error occurred accessing user data.',
+        });
+    }
+});
